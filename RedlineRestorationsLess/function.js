57316c48b0ae3158714df479a120d7f34c187fbc
Paul McMahon <colpanius@gmail.com>
2013-08-27 08:47:56 -0700

    Changes made on 8-26-13

--- RedlineRestorationsLess/function.js
+++ RedlineRestorationsLess/function.js
@@ -5,9 +5,13 @@ $(document).ready(function(){
 	$('.up-arrow-hover').click(function(){
 		movethismuch = $(this).parent().parent().find('.panel').height();
 		movethismuch = "+=" + movethismuch + "px";
-		$(this).parent().parent().find('.panel').animate({
-			'margin-top' : movethismuch
-		},300);
+		var arrowposition = $(this).offset();
+		if (arrowposition.top < 150){
+			$(this).parent().parent().find('.panel').animate({
+				'margin-top' : movethismuch
+			},300);
+		}
+		
 	});
 
 	$('.down-arrow-hover').click(function(){
@@ -16,6 +20,20 @@ $(document).ready(function(){
 		$(this).parent().parent().find('.panel').animate({
 			'margin-top' : movethismuch
 		},300);
+	});
+
+	$('.up-arrow-hover').hover(function(){
+		$(this).find('h6').css('display','block');
+	},
+	function(){
+		$(this).find('h6').css('display','none');
+	});
+
+	$('.down-arrow-hover').hover(function(){
+		$(this).find('h6').css('display','block');
+	},
+	function(){
+		$(this).find('h6').css('display','none');
 	});
 
 	//Control left and right arrow buttons
@@ -44,12 +62,23 @@ $(document).ready(function(){
 	function resizeDiv() {
 		vph = $(window).height();
 		vph = vph - 100;
-		$(".content-panel").css({"height": vph + "px"});
+		var trueheight = vph;
+		if (trueheight < 580) {
+			trueheight=580;
+		}
+		if (vph > 580) {
+			vph = 580;
+		}
+		else {
+			vph = 580;
+		}
+		$(".content-panel").css({"height": trueheight + "px"});
 		$(".panel").css({"height": vph + "px"});
-		$(".left-gutter").css({"height": vph + "px"});
-		$(".right-gutter").css({"height": vph + "px"});
+		$(".left-gutter").css({"height": trueheight + "px"});
+		$(".right-gutter").css({"height": trueheight + "px"});
 		$(".viewport-center").css({"height": vph + "px"});
 		$(".down-arrow-hover").css({"margin-top": vph - 53 + "px"});
+		$(".one").css('margin-top','0px');
 		$(".two").css({"margin-top": vph + "px"});
 		$(".three").css({"margin-top": vph * 2 + "px"});
 		$(".four").css({"margin-top": vph * 3 + "px"});
--- RedlineRestorationsLess/index.php
+++ RedlineRestorationsLess/index.php
@@ -4,97 +4,108 @@
 		<div class="content-panel-area">
 			<div class="one-big-page">
 				<div class="homepage panel one">
-					<img src="<?php echo get_template_directory_uri() ?>/images/slide1-car.png" class="slide-image" />
-					<div class="panel-textbar"></div>
+					<img src="<?php echo get_template_directory_uri(); ?>/images/slide1-car.png" class="slide-image" />
+					<div class="panel-textbar">
+						<img src="<?php echo get_template_directory_uri(); ?>/images/cross-flags.png" />
+						<h4>1964</h4>
+						<div class="panel-text-lines">
+							<h5>Aston Martin</h5>
+							<h5>DB5</h5>
+						</div>
+					</div>
 					<div class="down-arrow-hover">
 						<h6>The Process</h6>
-						<img src="<?php echo get_template_directory_uri() ?>/images/down-arrow.png" class="bottom-button" />
+						<img src="<?php echo get_template_directory_uri(); ?>/images/down-arrow.png" class="bottom-button" />
 					</div>
 				</div>
 				<div class="homepage panel two">
+					<img src="<?php echo get_template_directory_uri(); ?>/images/panel2-car.png" class="slide-image-horizontal" />
+					<div class="panel-textbar-horizontal"></div>
 					<div class="up-arrow-hover">
 						<h6>Home</h6>
-						<img src="<?php echo get_template_directory_uri() ?>/images/up-arrow.png" class="top-button" />
+						<img src="<?php echo get_template_directory_uri(); ?>/images/up-arrow.png" class="top-button" />
 					</div>
 					<div class="down-arrow-hover">
 						<h6>Who we are</h6>
-						<img src="<?php echo get_template_directory_uri() ?>/images/down-arrow.png" class="bottom-button" />
+						<img src="<?php echo get_template_directory_uri(); ?>/images/down-arrow.png" class="bottom-button" />
 					</div>
 				</div>
 				<div class="homepage panel three">
+					<img src="<?php echo get_template_directory_uri(); ?>/images/panel3-car.png" class="slide-image-wide" />
+					<div class="panel-textbar-wide"></div>
 					<div class="up-arrow-hover">
 						<h6>The Process</h6>
-						<img src="<?php echo get_template_directory_uri() ?>/images/up-arrow.png" class="top-button" />
+						<img src="<?php echo get_template_directory_uri(); ?>/images/up-arrow.png" class="top-button" />
 					</div>
 				</div>
 			</div>
 			<div class="one-big-page">
 				<div class="otherpage panel one">
-					<img src="<?php echo get_template_directory_uri() ?>/images/contact-slider-bg.jpg" class="slide-image" />
+					<img src="<?php echo get_template_directory_uri(); ?>/images/contact-slider-bg.jpg" class="slide-image" />
 					<div class="down-arrow-hover">
 						<h6>The Process</h6>
-						<img src="<?php echo get_template_directory_uri() ?>/images/down-arrow.png" class="bottom-button" />
+						<img src="<?php echo get_template_directory_uri(); ?>/images/down-arrow.png" class="bottom-button" />
 					</div>
 				</div>
 				<div class="otherpage panel two">
 					<div class="up-arrow-hover">
 						<h6>Home</h6>
-						<img src="<?php echo get_template_directory_uri() ?>/images/up-arrow.png" class="top-button" />
+						<img src="<?php echo get_template_directory_uri(); ?>/images/up-arrow.png" class="top-button" />
 					</div>
 					<div class="down-arrow-hover">
 						<h6>The Process</h6>
-						<img src="<?php echo get_template_directory_uri() ?>/images/down-arrow.png" class="bottom-button" />
+						<img src="<?php echo get_template_directory_uri(); ?>/images/down-arrow.png" class="bottom-button" />
 					</div>
 				</div>
 				<div class="otherpage panel three">
 					<div class="up-arrow-hover">
 						<h6>Home</h6>
-						<img src="<?php echo get_template_directory_uri() ?>/images/up-arrow.png" class="top-button" />
+						<img src="<?php echo get_template_directory_uri(); ?>/images/up-arrow.png" class="top-button" />
 					</div>
 					<div class="down-arrow-hover">
 						<h6>The Process</h6>
-						<img src="<?php echo get_template_directory_uri() ?>/images/down-arrow.png" class="bottom-button" />
+						<img src="<?php echo get_template_directory_uri(); ?>/images/down-arrow.png" class="bottom-button" />
 					</div>
 				</div>
 				<div class="otherpage panel four">
 					<div class="up-arrow-hover">
 						<h6>Home</h6>
-						<img src="<?php echo get_template_directory_uri() ?>/images/up-arrow.png" class="top-button" />
+						<img src="<?php echo get_template_directory_uri(); ?>/images/up-arrow.png" class="top-button" />
 					</div>
 				</div>
 			</div>
 			<div class="one-big-page">
 				<div class="otherpage panel one">
-					<img src="<?php echo get_template_directory_uri() ?>/images/RR_Slider_Image.jpg" class="slide-image" />
+					<img src="<?php echo get_template_directory_uri(); ?>/images/RR_Slider_Image.jpg" class="slide-image" />
 					<div class="down-arrow-hover">
 						<h6>The Process</h6>
-						<img src="<?php echo get_template_directory_uri() ?>/images/down-arrow.png" class="bottom-button" />
+						<img src="<?php echo get_template_directory_uri(); ?>/images/down-arrow.png" class="bottom-button" />
 					</div>
 				</div>
 				<div class="otherpage panel two">
 					<div class="up-arrow-hover">
 						<h6>Home</h6>
-						<img src="<?php echo get_template_directory_uri() ?>/images/up-arrow.png" class="top-button" />
+						<img src="<?php echo get_template_directory_uri(); ?>/images/up-arrow.png" class="top-button" />
 					</div>
 					<div class="down-arrow-hover">
 						<h6>The Process</h6>
-						<img src="<?php echo get_template_directory_uri() ?>/images/down-arrow.png" class="bottom-button" />
+						<img src="<?php echo get_template_directory_uri(); ?>/images/down-arrow.png" class="bottom-button" />
 					</div>
 				</div>
 				<div class="otherpage panel three">
 					<div class="up-arrow-hover">
 						<h6>Home</h6>
-						<img src="<?php echo get_template_directory_uri() ?>/images/up-arrow.png" class="top-button" />
+						<img src="<?php echo get_template_directory_uri(); ?>/images/up-arrow.png" class="top-button" />
 					</div>
 					<div class="down-arrow-hover">
 						<h6>The Process</h6>
-						<img src="<?php echo get_template_directory_uri() ?>/images/down-arrow.png" class="bottom-button" />
+						<img src="<?php echo get_template_directory_uri(); ?>/images/down-arrow.png" class="bottom-button" />
 					</div>
 				</div>
 				<div class="otherpage panel four">
 					<div class="up-arrow-hover">
 						<h6>Home</h6>
-						<img src="<?php echo get_template_directory_uri() ?>/images/up-arrow.png" class="top-button" />
+						<img src="<?php echo get_template_directory_uri(); ?>/images/up-arrow.png" class="top-button" />
 					</div>
 				</div>
 			</div>
@@ -102,33 +113,33 @@
 				<div class="otherpage panel one">
 					<div class="down-arrow-hover">
 						<h6>The Process</h6>
-						<img src="<?php echo get_template_directory_uri() ?>/images/down-arrow.png" class="bottom-button" />
+						<img src="<?php echo get_template_directory_uri(); ?>/images/down-arrow.png" class="bottom-button" />
 					</div>
 				</div>
 				<div class="otherpage panel two">
-				<div class="up-arrow-hover">
+					<div class="up-arrow-hover">
 						<h6>Home</h6>
-					<img src="<?php echo get_template_directory_uri() ?>/images/up-arrow.png" class="top-button" />
+						<img src="<?php echo get_template_directory_uri(); ?>/images/up-arrow.png" class="top-button" />
 					</div>
 					<div class="down-arrow-hover">
 						<h6>The Process</h6>
-						<img src="<?php echo get_template_directory_uri() ?>/images/down-arrow.png" class="bottom-button" />
+						<img src="<?php echo get_template_directory_uri(); ?>/images/down-arrow.png" class="bottom-button" />
 					</div>
 				</div>
 				<div class="otherpage panel three">
-				<div class="up-arrow-hover">
+					<div class="up-arrow-hover">
 						<h6>Home</h6>
-					<img src="<?php echo get_template_directory_uri() ?>/images/up-arrow.png" class="top-button" />
+						<img src="<?php echo get_template_directory_uri(); ?>/images/up-arrow.png" class="top-button" />
 					</div>
 					<div class="down-arrow-hover">
 						<h6>The Process</h6>
-						<img src="<?php echo get_template_directory_uri() ?>/images/down-arrow.png" class="bottom-button" />
+						<img src="<?php echo get_template_directory_uri(); ?>/images/down-arrow.png" class="bottom-button" />
 					</div>
 				</div>
 				<div class="otherpage panel four">
-				<div class="up-arrow-hover">
+					<div class="up-arrow-hover">
 						<h6>Home</h6>
-					<img src="<?php echo get_template_directory_uri() ?>/images/up-arrow.png" class="top-button" />
+						<img src="<?php echo get_template_directory_uri(); ?>/images/up-arrow.png" class="top-button" />
 					</div>
 				</div>
 			</div>
